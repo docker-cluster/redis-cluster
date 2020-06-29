3718ed24336150a2ef0e12cf718e6d63db9e491e
Dongdong Zhang <dongdzha@cisco.com>
2020-06-29 21:06:01 +0800

    first commit

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "***************************start to init redis-cluster containers***************************"
+docker-compose down
+rm -rf redis-*
+mkdir {redis-master{1..3},redis-slave{1..3}}
+for F in {redis-master{1..3},redis-slave{1..3}} ;do cp -fv redis.conf $F ;done
+mytest=36379 ;for F in {redis-master{1..3},redis-slave{1..3}} ;do sed -i "s/6379/$mytest/" $F/redis.conf ;mytest=$(expr $mytest + 1) ;done
+docker-compose up -d
+echo "***************************redis-cluster containers inited***************************"
+
+echo "***************************start to connect redis-cluster***************************"
+local_host="`hostname --fqdn`"
+local_ip=`host $local_host 2>/dev/null | awk '{print $NF}'`
+docker run --rm -it inem0o/redis-trib create --replicas 1 $local_ip:363{79..84}
+yes
+echo "***************************redis-cluster connected***************************"
+
+exit 0
